b14c2b96c2fa56d699851e255f0f0852faa3384b
KYOH95 <kakasi18@naver.com>
2021-09-28 16:53:00 -0500

    2021_Sep_28

--- DFS_BFS/ndb_002.py
+++ DFS_BFS/ndb_002.py
@@ -7,20 +7,44 @@
 #   000001
 #   111111
 #   111111
+from collections import deque 
+
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    # print(queue)
+ 
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+
+    return graph[N-1][M-1]
+                
 
 N,M = map(int,input().split())
 
 graph = []
 for i in range(N):
-    graph.append(list(map(int,(input()))))
+    graph.append(list(map(int, input())))
+
+#    상 하 좌 우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 
-def dfs(x,y):
-    pass
-
-
-count = 0
-for i in range(N):
-    for j in range(M):
-        if dfs(i,j) == True:
-            count += 1
+print(bfs(0,0))
+print(graph)
