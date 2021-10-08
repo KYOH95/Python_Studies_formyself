b2edcaa6e585bf3947f6c1e418bea04d5849c77e
KYOH95 <kakasi18@naver.com>
2021-10-07 23:55:51 -0500

    2021_Oct_7

---
+++ Greedy_Problems/boj_12904.py
@@ -0,0 +1,24 @@
+#A와 B
+
+#input: B           output:
+#       ABBA
+
+# S를 T로 바꿀 수 있으면 1, 없으면 0
+
+S = input()
+T = input()
+
+#문자열 뒤에 A를 추가
+#문자열을 뒤집고 뒤에 B를 추가
+
+while True:
+    if len(S) == len(T):
+        if S == T: print(1)
+        else: print(0)
+        break
+    if T[len(T)-1]== 'A':
+        T = T[:-1]
+    else:
+        T = T[:-1]
+        T = T[::-1]
+
--- blank.py
+++ blank.py
@@ -1,14 +1,10 @@
 #practice
 
 
+T = 'ABC'
 
-# def is_vowel(x):
-#     return x in "aeiou"
+T = T[:-1]
 
-# print(is_vowel('a'))
+T = T[::-1]
 
-listA = [['aaa','bbb','ccc'],['a','b','c']]
-def check(str_x):
-    return str_x in listA
-
-print(check(['a','b']))
+print(T)
