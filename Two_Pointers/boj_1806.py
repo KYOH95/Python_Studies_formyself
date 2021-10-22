23dd6ae8d77f73807aaa87969bdc16d30654fcc9
KYOH95 <kakasi18@naver.com>
2021-10-21 23:38:31 -0500

    2021_Oct_21

---
+++ Two_Pointers/boj_1806.py
@@ -0,0 +1,22 @@
+#boj_1806
+#부분합
+
+import sys
+si = sys.stdin.readline
+
+N, S = 10, 15
+#5 1 3 5 10 7 4 9 2 8
+N_list = list(map(int,si().split()))
+
+
+
+# R = 0
+# for x in N_list:
+#     sum += 
+
+# for L in N_list:
+#     sum += L
+
+
+#     for R in N_list:
+
