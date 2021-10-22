03535ed1e295594d73ab231adbaf8cfa5125f200
KYOH95 <kakasi18@naver.com>
2021-10-22 16:56:18 -0500

    2021_Oct_22

--- Two_Pointers/boj_1806.py
+++ Two_Pointers/boj_1806.py
@@ -8,15 +8,18 @@ N, S = 10, 15
 #5 1 3 5 10 7 4 9 2 8
 N_list = list(map(int,si().split()))
 
+R, sum, ans = -1, 0, N + 1
+for L in range(N):
+    while R + 1 < N and sum < S:
+        R += 1
+        sum += N_list[R]
+    
+    if sum >= S:
+        ans = min(ans, R - L + 1)
 
+    sum -= N_list[L]
 
-# R = 0
-# for x in N_list:
-#     sum += 
+if ans == N + 1: ans = 0
+print(ans)
 
-# for L in N_list:
-#     sum += L
-
-
-#     for R in N_list:
 
