7da137386834e492ba187cca3bb1ef93449816a3
KYOH95 <kakasi18@naver.com>
2021-11-21 18:55:58 -0600

    2021_Nov_21

--- Greedy_Problems/boj_1541.py
+++ Greedy_Problems/boj_1541.py
@@ -1,23 +1,34 @@
 #잃어버린 괄호
 #input = 55-50+40       output: -35
-#!실패
+import sys
+si = sys.stdin.readline
 
-
-s = input()
-
-s_temp = s.replace('+','-')
-
-s_number = s_temp.split('-')
-s_plus_minus = []
+s = si()
+temp = ''
+ans = 0
+last_op = True #if Ture, possitive
 
 for i in range(0,len(s)):
-    if s[i] =='-':
-        s_plus_minus.append(0)
-    elif s[i] =='+':
-        s_plus_minus.append(1)
+    # When operator
+    if s[i] == '+' or s[i] == '-':
+        if last_op == True:
+            ans += int(temp)
+        else:
+            ans -= int(temp)
+        temp = ''
+        if s[i] == '-': last_op = False
+        # print(f'ans = ',ans)
 
-print(s_plus_minus)
-data = []
+    #when number
+    else: 
+        temp += s[i]
 
+    #for the last numbers
+    if i == len(s)-1:
+        if last_op == True:
+            ans += int(temp)
+        else:
+            ans -= int(temp)
 
-print(data)
+print(ans)
+
