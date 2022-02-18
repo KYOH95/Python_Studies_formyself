036c180ccdb07232d0d064498601b9fd0fee4456
KYOH95 <kakasi18@naver.com>
2022-02-17 23:34:10 -0600

    2022_Feb_17

--- Programmers_Q/_deletePair.py
+++ Programmers_Q/_deletePair.py
@@ -2,37 +2,24 @@
 짝지어 제거하기
 
 """
+
 from collections import deque
 def solution(s):
     answer = 0
+    s_list = [s[0]]
     
-    Q = deque(s)
-    boolCheck = False
-    while len(Q)>1:
-        temp = []
-
-        while True:
-            count = len(Q)
-            if len(Q) == 1:
-                temp.append(Q[0])
-                break
-            
-            if Q[0] == Q[1]:
-                Q.popleft()
-                Q.popleft()
+    for x in s[1:]:
+        if len(s_list)==0:
+            s_list.append(x)
+        else:
+            if x == s_list[-1]:
+                s_list.pop()
             else:
-                temp.append(Q[0])
-                Q.popleft()
-
-            if count == len(Q):
-                boolCheck = True
-            if len(Q) == 0: break
-        
-        Q = deque(temp)
-        if boolCheck == True: break
-        
-    if len(Q)==0:
-        answer = 1
+                s_list.append(x)
     
+    if len(s_list) == 0: 
+        answer = 1
     return answer
+
+print(solution("bbabba"))
 print(solution("cdcd"))
