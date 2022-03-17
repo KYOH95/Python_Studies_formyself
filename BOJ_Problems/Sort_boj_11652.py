c83c3c8d041b5e5ea012924c15a032c3ef728f21
KYOH95 <kakasi18@naver.com>
2022-03-17 17:04:19 -0500

    2022_Mar_17

--- BOJ_Problems/Sort_boj_11652.py
+++ BOJ_Problems/Sort_boj_11652.py
@@ -14,4 +14,20 @@ for i in range(N):
 
 list_.sort()
 
-print(list_)
+count = 0
+max = 0
+answer = 0
+for i in range(1,N):
+    if list_[i] == list_[i-1]:
+        count += 1
+    else: 
+        if count > max:
+            max = count
+            answer = list_[i-1]
+        count = 0
+
+if count > max:
+            max = count
+            answer = list_[i-1]
+
+print(answer)
