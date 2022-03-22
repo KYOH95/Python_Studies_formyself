36922a90aa0798892977cfb49686be9096658c07
KYOH95 <kakasi18@naver.com>
2022-03-22 18:11:12 -0500

    2022_Mar_22

--- BOJ_Problems/BS_boj_2470.py
+++ BOJ_Problems/BS_boj_2470.py
@@ -5,20 +5,31 @@ Link: https://www.acmicpc.net/problem/2470
 
 import sys
 si = sys.stdin.readline
-
 N = int(si())
 A = list(map(int,si().split()))
 A.sort()
-
 def BS(listA,L,R,X):
-    while L < R:
+    result = R+1
+    while L<=R:
         mid = (L+R)//2
-        if listA[mid] > -X:
-            R = mid-1
-        elif listA[mid] <= -X:
-            L = mid+1
-    print(X,listA[mid])
+        if listA[mid] < X:
+            L = mid + 1
+        else:
+            result = mid
+            R = mid - 1
+    return result
 
-print(A)
-for i in range(1,len(A)-1):
-    BS(A,i,len(A)-1,A[i-1])
+sum_ = 1 << 31
+ans1, ans2 = 0,0
+for i in range(N-1):
+    cand = BS(A,i+1,N-1,-A[i])
+    if cand < N and abs(A[i]+A[cand]) < sum_:
+        sum_ = abs(A[i]+A[cand])
+        ans1, ans2 = A[i],A[cand]
+    if cand -1 > i and abs(A[i]+A[cand-1]) < sum_:
+        sum_ = abs(A[i]+A[cand-1])
+        ans1, ans2 = A[i],A[cand-1]
+
+print(ans1,ans2)
+
+    
