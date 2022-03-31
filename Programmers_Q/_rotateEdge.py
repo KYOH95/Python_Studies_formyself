586b17d79d674c710d82f92d8dbdab35c73df656
KYOH95 <kakasi18@naver.com>
2022-03-31 00:19:46 -0500

    2022_Mar_31

--- Programmers_Q/_rotateEdge.py
+++ Programmers_Q/_rotateEdge.py
@@ -3,32 +3,42 @@
 link: https://programmers.co.kr/learn/courses/30/lessons/77485
 """
 
-
 def rotate(map,q):
+    temp = map[q[0]-1][q[1]-1]
+    min_list = [map[q[0]-1][q[1]-1]]
+    #left side
+    for i in range(q[0]-1,q[2]-1):
+        map[i][q[1]-1] = map[i+1][q[1]-1]
+        min_list.append(map[i+1][q[1]-1])
 
-    print(q[0],q[1],q[2],q[3])
-    temp = map[q[1]-1][q[3]-1]
-    for i in range(q[2]-1,q[0]-2,-1):
-        print(map[i][q[1]])
+    #bottom side
+    for i in range(q[1]-1,q[3]-1):
+        map[q[2]-1][i] = map[q[2]-1][i+1]
+        min_list.append(map[q[2]-1][i+1])
 
+    #right side
+    for i in range(q[2]-1,q[0]-1,-1):
+        map[i][q[3]-1] = map[i-1][q[3]-1]
+        min_list.append(map[i-1][q[3]-1])
+
+    #top side
+    for i in range(q[3]-1,q[1]-1,-1):
+        map[q[0]-1][i] = map[q[0]-1][i-1]
+        min_list.append(map[q[0]-1][i-1])
+    map[q[0]-1][q[1]] = temp
+    return min(min_list)
 
 def solution(rows, columns, queries):
     answer = []
     map = [[0 for _ in range(columns)] for _ in range(rows)] 
-    count = 1
+    count = 0
     for i in range(rows):
         for j in range(columns):
-            map[i][j] = count
             count +=1
-    
-    rotate(map,queries[0])
-        
-
-
-
-
-
+            map[i][j] = count
+    for i in range(len(queries)):
+        answer.append((rotate(map,queries[i])))
     return answer
 
 
-solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]])
+print(solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
