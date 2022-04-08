d5fe634fb499012a04ef704491f219ce09099831
KYOH95 <kakasi18@naver.com>
2022-04-08 16:11:59 -0500

    2022_Apr_8

--- BOJ_Problems/TP_boj_16472.py
+++ BOJ_Problems/TP_boj_16472.py
@@ -3,33 +3,31 @@
 link: https://www.acmicpc.net/problem/16472
 """
 
-from collections import deque
 import sys
 si = sys.stdin.readline
 
 N = int(si())
 word = si()[:-1]
-dict = {}
+count = [0] * 200
+kind = 0
 
-sum = 0
+def add(x):
+    global kind
+    count[ord(x)] += 1
+    if count[ord(x)] == 1: kind += 1
+
+def sub(x):
+    global kind
+    count[ord(x)] -= 1
+    if count[ord(x)] == 0: kind -= 1
+
+L = 0
 ans = 0
-R = 0
-for L in range(len(word)):
-    ans = max(sum,ans)
-    while R < len(word) and len(dict)<= N:
-        R += 1
-        if word[R] in dict:
-            dict[word[R]] += 1
-        else:
-            dict[word[R]] = 0
-        
-        sum += 1
-    print(sum)
-    sum -= 1
-    dict[word[L]] -= 1
-    if word[L] == 0: del dict[word[L]]
+for R in range(len(word)):
+    add(word[R])
+    while kind > N:
+        sub(word[L])
+        L+=1
+    ans = max(ans, R-L+1)
 
 print(ans)
-
-
-    
