b73837198faa122b3b1847d404822c21eb5016d7
KYOH95 <kakasi18@naver.com>
2022-10-12 15:20:48 +0900

    2022_Oct_12

--- CodingTest_Programmers/2022_10_07_2_JadenCase.py
+++ CodingTest_Programmers/2022_10_07_2_JadenCase.py
@@ -1,15 +1,15 @@
+# JadenCase 문자열 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
 def solution(s):
     answer = ''
-    word_list = " ".join(map(str, s.split(" ")))
+    word_list = list(map(str, s.split(" ")))
     
-    for i in range(len(word_list)):
-        # if 
-        if word_list[i].isupper(): 
-            answer += word_list[i].lower()
-        else:
-            answer += word_list[i]
-
-    return answer
+    for word in word_list:
+        answer += word.capitalize()
+        answer += ' '
+            
+    return answer[:-1]
 
 
 print(solution("3people unFollowed me"))
